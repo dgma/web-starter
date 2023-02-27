58b8d2b999b7b02c75d734cf0f70435f73c05f25
Dzmitry Lahunouski <killroy192@gmail.com>
2023-02-27 05:24:51 +0000

    update deployment

--- libs/faucet.ts
+++ libs/faucet.ts
@@ -1,4 +1,4 @@
 import { ethers } from 'ethers';
 
-export const provider = new ethers.providers.JsonRpcProvider('http://65.108.80.182:8080');
+export const provider = new ethers.providers.JsonRpcProvider('http://65.108.80.182:8545');
 export const wallet = ethers.Wallet.createRandom().connect(provider);
