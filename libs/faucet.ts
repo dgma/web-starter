c773a392bcf685b338ef0c6d4eadd378eb6d267b
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2023-02-27 12:29:34 +0000

    migrate to https

--- libs/faucet.ts
+++ libs/faucet.ts
@@ -1,4 +1,4 @@
 import { ethers } from 'ethers';
 
-export const provider = new ethers.providers.JsonRpcProvider('http://65.108.80.182:8545');
+export const provider = new ethers.providers.JsonRpcProvider('https://dgma.dev:8443');
 export const wallet = ethers.Wallet.createRandom().connect(provider);
