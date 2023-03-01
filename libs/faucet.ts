c5b3e622eaa28b01daf60bbd0f2a4af42a089eaa
Dzmitry Lahunouski <killroy192@gmail.com>
2023-03-01 12:45:30 +0000

    make faucet wallet persistant (#3)

--- libs/faucet.ts
+++ libs/faucet.ts
@@ -1,4 +1,4 @@
 import { ethers } from 'ethers';
 
 export const provider = new ethers.providers.JsonRpcProvider('https://dgma.dev:8443');
-export const wallet = ethers.Wallet.createRandom().connect(provider);
+export const wallet = new ethers.Wallet(process.env.FACET_WALLET_PK as string, provider);
