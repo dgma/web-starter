0a3c01187345bfbf3feeccc96d19663c39fe90ab
Dzianis Roi <mcreader215@gmail.com>
2023-03-13 16:29:07 +0000

    Added errors reporting (#7)
    * Added errors reporting
    
    * Added ability to disable reporting
    
    * Added ability to disable reporting
    
    * Added Sentry secrets for production pipeline

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  sentry: {
+    hideSourceMaps: true,
+    disableServerWebpackPlugin: !process.env.SENTRY_DSN,
+    disableClientWebpackPlugin: !process.env.NEXT_PUBLIC_SENTRY_DSN,
+  }
 }
 
-module.exports = nextConfig
+module.exports = withSentryConfig(
+  nextConfig,
+  { silent: true },
+);
