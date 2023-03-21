af8b4ad48847657436119a6eb3408290b70014b8
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2023-03-21 18:05:13 +0100

    implement version handle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import pkg from 'package.json';
 import type { AppProps } from 'next/app'
 
 import { AppProvider } from '@/libs/context/app';
 import 'react-toastify/dist/ReactToastify.css';
+
+console.log('version:', pkg.version);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
