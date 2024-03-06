fe21af44ecf1c3bd5f5f1321ffcc42a9605be437
Dzmitry Lahunouski <killroy192@gmail.com>
2024-03-06 23:04:50 +0000

    init setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { config } from "./config";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div className="dark:text-white text-black">Hello World</div>;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <div className="dark:text-white text-black">Hello World</div>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
 
 export default App;
---
+++ src/config.ts
@@ -0,0 +1,15 @@
+import { http, createConfig } from "wagmi";
+import { hardhat } from "wagmi/chains";
+
+declare module "wagmi" {
+  interface Register {
+    config: typeof config;
+  }
+}
+
+export const config = createConfig({
+  chains: [hardhat],
+  transports: {
+    [hardhat.id]: http(),
+  },
+});
