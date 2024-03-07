8bb94393439a1690f530f7432b6b61f6d17c2245
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2024-03-07 17:32:20 +0000

    add web3modal connecter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import { config } from "./config";
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { config, projectId } from "./config";
 
 const queryClient = new QueryClient();
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: false,
+});
 
 function App() {
   return (
